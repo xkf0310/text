0323fd80462e24bbc642a3d76f51aa017aaecf6d
xkf0310 <50689303+xkf0310@users.noreply.github.com>
2020-01-11 20:11:31 +0800

    hellowpush

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class hello_world{
+    public static void main(String[] args){
+		System.out.println("Hello aaa!");
+        System.out.println("Hello world!");  
+		System.out.println("Hello bbbb!");
+		System.out.println("Hello cccc");
+    }        
+}
